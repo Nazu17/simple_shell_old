f1719436e426bd201e25791806543d7b65ed03ac
Nazrawi Asrat <nasrat2676@gmail.com>
2023-09-22 06:55:49 -0700

    Betty would be proud

---
+++ _atoi.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program.
+ *
+ * This program prints the message "Stop, war on religion!"
+ * to the standard output.
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	char *message = "Stop, war on religion!";
+
+	printf("%s\n", message);
+
+	return (0);
+}
