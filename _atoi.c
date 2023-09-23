a12090f6c2c64596e098ed4431aef30534b8e690
Nazrawi Asrat <nasrat2676@gmail.com>
2023-09-23 07:35:31 -0700

    version 2

--- _atoi.c
+++ _atoi.c
@@ -1,19 +1,45 @@
-#include <stdio.h>
+#include "shell.h"
+
+void help_env(void);
+void help_setenv(void);
+void help_unsetenv(void);
+void help_history(void);
 
 /**
- * main - entry point of the program.
- *
- * This program prints the message "Stop, war on religion!"
- * to the standard output.
- *
- * Return: always 0 (Success)
+ * help_env - Displays information on the shellby builtin command 'env'.
  */
-
-int main(void)
+void help_env(void)
 {
-	char *message = "Stop, war on religion!";
+	char *msg = "env: env\n\tPrints the current environment.\n";
 
-	printf("%s\n", message);
+	write(STDOUT_FILENO, msg, _strlen(msg));
+}
 
-	return (0);
+/**
+ * help_setenv - Displays information on the shellby builtin command 'setenv'.
+ */
+void help_setenv(void)
+{
+	char *msg = "setenv: setenv [VARIABLE] [VALUE]\n\tInitializes a new";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "environment variable, or modifies an existing one.\n\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "\tUpon failure, prints a message to stderr.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+}
+
+/**
+ * help_unsetenv - Displays information on the shellby builtin command
+ * 'unsetenv'.
+ */
+void help_unsetenv(void)
+{
+	char *msg = "unsetenv: unsetenv [VARIABLE]\n\tRemoves an ";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "environmental variable.\n\n\tUpon failure, prints a ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "message to stderr.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
 }
--- exits.c
+++ exits.c
@@ -1,32 +1,12 @@
 #include "shell.h"
 
 /**
- * *_strchr - locates a character in a string.
- * @s: the string to be parsed.
- * @c: the character to look for.
- *
- * Return: (s) pointer to the memory area s.
+ **_strncpy - copies a string
+ *@dest: the destination string to be copied to
+ *@src: the source string
+ *@n: the amount of characters to be copied
+ *Return: the concatenated string
  */
-
-char *_strchr(char *s, char c)
-{
-	do {
-		if (*s == c)
-			return (s);
-	} while (*s++ != '\0');
-
-	return (NULL);
-}
-
-/**
- * *_strncpy - copies string.
- * @dest: the destination string to be copied
- * @src: source string.
- * @n: amount of characters to be copied.
- *
- * Return: the concatenated string.
- */
-
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i, j;
@@ -51,14 +31,12 @@ char *_strncpy(char *dest, char *src, int n)
 }
 
 /**
- * *_strncat - concatenates two strings.
- * @dest: the first string.
- * @src: second string.
- * @n: amount of bytes to be maximally used.
- *
- * Return: concatenated string.
+ **_strncat - concatenates two strings
+ *@dest: the first string
+ *@src: the second string
+ *@n: the amount of bytes to be maximally used
+ *Return: the concatenated string
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
@@ -66,7 +44,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	i = 0;
 	j = 0;
-	while (dest[i] !=  '\0')
+	while (dest[i] != '\0')
 		i++;
 	while (src[j] != '\0' && j < n)
 	{
@@ -77,4 +55,20 @@ char *_strncat(char *dest, char *src, int n)
 	if (j < n)
 		dest[i] = '\0';
 	return (s);
+}
+
+/**
+ **_strchr - locates a character in a string
+ *@s: the string to be parsed
+ *@c: the character to look for
+ *Return: (s) a pointer to the memory area s
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
 }
